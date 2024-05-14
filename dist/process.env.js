22a15f5f54aa54ebeefee6294188aba24266f144
HARDIntegral <mohammed.a.tazwar04@gmail.com>
2024-05-14 02:00:10 -0400

    help

--- dist/process.env.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DISCORD_TOKEN = void 0;
-exports.DISCORD_TOKEN = 'MTIzOTA3OTAwMDMzNzQyMDM1OA.Ge7wLN.mDxI4an-vwPft9WEf-j_B8dhhuUlClK6DHsTfY';
